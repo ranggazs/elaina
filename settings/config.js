9be95f426bd75b5d4b12008b15bf8ba9e8c4f4e2
ranggazs <74724682+ranggazs@users.noreply.github.com>
2022-03-13 19:19:24 +0800

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -2,17 +2,17 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 // self or public
-global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
+global.self = false //jadiin true klo ga mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="RafliKunn"
-global.ownernumber = "62895410068780"
-global.botname = "RflBotz Ofc"
+global.ownername ="Alfareza"
+global.ownernumber = "6285157770338"
+global.botname = "Alfa"
 global.thumbnail = fs.readFileSync("./image/elaina.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'rafliofcheh' //ganti pakai apikey kalian
 global.limit = {
-		free:20,
+		free:50,
 		premium:1000
 	}
 global.session_name = "session.json"
